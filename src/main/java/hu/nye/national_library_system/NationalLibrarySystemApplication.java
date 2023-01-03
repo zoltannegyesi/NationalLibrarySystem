5dd8aef5c798cb819e30be7dd1f4def28367b221
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-01-03 15:03:44 +0100

    Initial

---
+++ src/main/java/hu/nye/national_library_system/NationalLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.national_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/national_library_system/NationalLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.national_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
