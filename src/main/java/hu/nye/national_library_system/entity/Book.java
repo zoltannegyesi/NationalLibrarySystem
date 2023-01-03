1c6cfcc919d8c5a9529818b2b981657df1c2fb42
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-01-03 18:00:02 +0100

    Book

---
+++ src/main/java/hu/nye/national_library_system/entity/Book.java
@@ -0,0 +1,44 @@
+package hu.nye.national_library_system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    private String isbn;
+
+    @Column(name = "eto")
+    private Double eto;
+
+    @Column(name = "title", length = 100)
+    private String title;
+
+    @Column(name = "author", length = 100)
+    private String author;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "number_of_pages")
+    private Long numberOfPages;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "description")
+    private ;
+
+    @Column(name = "")
+    private ;
+}
