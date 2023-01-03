6c2a2c4682e5b9b1807ae573616d6b3bc5b63563
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-01-03 18:27:32 +0100

    Library

---
+++ src/main/java/hu/nye/national_library_system/entity/Library.java
@@ -0,0 +1,34 @@
+package hu.nye.national_library_system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @Column(name = "open_time")
+    private LocalTime open_time;
+
+    @Column(name = "close_time")
+    private LocalTime close_time;
+}
