fb029b6f8e8a0433eb823b9d0958b80f9ca7d44f
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-01-12 18:20:55 +0100

    FractionFilters

--- src/main/java/hu/nye/national_library_system/filter/FractionFilter.java
+++ src/main/java/hu/nye/national_library_system/filter/FractionFilter.java
@@ -4,21 +4,27 @@ import hu.nye.national_library_system.Constants;
 import hu.nye.national_library_system.etc.QueryOperator;
 
 import javax.persistence.criteria.*;
-import java.time.LocalDate;
+import java.math.BigDecimal;
 
-public class BooleanFilter extends Filter<Boolean> {
+public class FractionFilter extends Filter<BigDecimal> {
 
-    public BooleanFilter(String fieldName, QueryOperator queryOperator, Boolean value) {
+    public FractionFilter(String fieldName, QueryOperator queryOperator, BigDecimal value) {
         super(fieldName, queryOperator, value);
     }
 
     @Override
     public Predicate getPredicate(CriteriaBuilder criteriaBuilder, Root<?> root, String fieldName) {
         Predicate fieldPredicate = null;
-        Path<Boolean> fieldPath = root.get(fieldName);
+        Path<BigDecimal> fieldPath = root.get(fieldName);
         switch (queryOperator) {
             case EQUALS:
                 fieldPredicate = criteriaBuilder.equal(fieldPath, value);
+                break;
+            case LESS_THAN_OR_EQUALS:
+                fieldPredicate = criteriaBuilder.le(fieldPath, value);
+                break;
+            case GREATER_THAN_OR_EQUALS:
+                fieldPredicate = criteriaBuilder.ge(fieldPath, value);
                 break;
             default:
                 fieldPredicate = null;
