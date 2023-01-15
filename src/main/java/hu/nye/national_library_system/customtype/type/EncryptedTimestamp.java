992059ed9d4e648e3936415dab80fbc797659b6e
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-01-15 15:32:36 +0100

    --ammend

--- src/main/java/hu/nye/national_library_system/customtype/type/EncryptedTimestamp.java
+++ src/main/java/hu/nye/national_library_system/customtype/type/EncryptedTimestamp.java
@@ -1,2 +1,25 @@
-package hu.nye.national_library_system.customtype.type;public class EncryptedTimestamp {
+package hu.nye.national_library_system.customtype.type;
+
+import hu.nye.national_library_system.customtype.descriptor.EncryptedTimestampDescriptor;
+import org.hibernate.type.descriptor.sql.BlobTypeDescriptor;
+
+import java.time.LocalDateTime;
+import java.util.Properties;
+
+public class EncryptedTimestamp extends  Encrypted<LocalDateTime> {
+
+    public EncryptedTimestamp() {
+        super(BlobTypeDescriptor.DEFAULT, new EncryptedTimestampDescriptor());
+    }
+
+    @Override
+    public String getName() {
+        return "EncryptedTimestamp";
+    }
+
+    @Override
+    public void setParameterValues(Properties parameters) {
+        setParameterValues(EncryptedTimestampDescriptor.class, parameters);
+    }
+
 }
